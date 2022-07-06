e48c5ad6c5db84555d3b08b3d7d0035cd39e3d6d
jprudolph03 <63522574+jprudolph03@users.noreply.github.com>
2022-07-06 17:25:37 -0400

    built new part form

--- components/NewPartForm.js
+++ components/NewPartForm.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Form, Button } from "semantic-ui-react";
 
 const NewPartForm = () => {
-  return <div>NewPartForm</div>;
+  const handleNewPartSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Form onSubmit={handleNewPartSubmit}>
+      <Form.Field>
+        <label>New Part Name</label>
+        <input placeholder="part name" />
+      </Form.Field>
+      <Form.Field>
+        <label>Part Prefix</label>
+        <input placeholder="part prefix" type="number" step={"0.01"} />
+      </Form.Field>
+      <Form.Field>
+        <label>Coil Loss</label>
+        <input placeholder="coil loss" type="number" step={"0.01"} />
+      </Form.Field>
+      <Button type="submit" color="green">
+        Submit
+      </Button>
+    </Form>
+  );
 };
 
 export default NewPartForm;
--- pages/addNewPart.js
+++ pages/addNewPart.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Header, Icon } from "semantic-ui-react";
 
+import NewPartForm from "../components/NewPartForm";
+
 const addNewPart = () => {
   return (
     <Header as="h2">
       <Icon name="plus circle" />
       <Header.Content>Add New Part</Header.Content>
       <hr />
+      <NewPartForm />
     </Header>
   );
 };
