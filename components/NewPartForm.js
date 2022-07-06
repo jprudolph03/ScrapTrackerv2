455e2c7798fe0379560a851bbf17b4eac39bb25b
jprudolph03 <63522574+jprudolph03@users.noreply.github.com>
2022-07-06 17:12:21 -0400

    new part form

---
+++ components/NewPartForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPartForm = () => {
+  return <div>NewPartForm</div>;
+};
+
+export default NewPartForm;
--- pages/addNewPart.js
+++ pages/addNewPart.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Header, Icon } from "semantic-ui-react";
 
 const addNewPart = () => {
-  return <div>Suckit</div>;
+  return (
+    <Header as="h2">
+      <Icon name="plus circle" />
+      <Header.Content>Add New Part</Header.Content>
+      <hr />
+    </Header>
+  );
 };
 
 export default addNewPart;
