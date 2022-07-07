43d66e1473eff6d90c6bc19f98429c8b154262ee
jprudolph03 <63522574+jprudolph03@users.noreply.github.com>
2022-07-07 15:38:38 -0400

    fix layout issue

--- pages/addNewPart.js
+++ pages/addNewPart.js
@@ -5,12 +5,14 @@ import NewPartForm from "../components/NewPartForm";
 
 const addNewPart = () => {
   return (
-    <Header as="h2">
-      <Icon name="plus circle" />
-      <Header.Content>Add New Part</Header.Content>
+    <>
+      <Header as="h2">
+        <Icon name="plus circle" />
+        <Header.Content>Add New Part</Header.Content>
+      </Header>
       <hr />
       <NewPartForm />
-    </Header>
+    </>
   );
 };
 
